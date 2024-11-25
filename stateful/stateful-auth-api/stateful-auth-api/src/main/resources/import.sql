3c7b6bac605d420ffb21b15b15be78072048cba5
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-11-25 18:19:16 -0300

    Conectando as aplicações ao PostgreSQL

---
+++ stateful/stateful-auth-api/stateful-auth-api/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO public.user (id, username, password) VALUES (1, 'user.test', '$2a$10$76hoqxAbdvWoxB.gEagI1uMQlyGbD7a/DhCKYv7MZss4Mq1RJ3J4u')
+
+INSERT INTO public.user (id, username, password) VALUES (2, 'user.new.test', '$2a$10$76hoqxAbdvWoxB.gEagI1uMQlyGbD7a/DhCKYv7MZss4Mq1RJ3J4u')
--- stateless/stateless-auth-api/stateless-auth-api/src/main/java/br/com/microservice/stateless_auth_api/StatelessAuthApiApplication.java
+++ stateless/stateless-auth-api/stateless-auth-api/src/main/java/br/com/microservice/stateless_auth_api/StatelessAuthApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatelessAuthApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StatelessAuthApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StatelessAuthApiApplication.class, args);
+    }
 
 }
